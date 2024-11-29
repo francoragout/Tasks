8999dff65954cb76048a2b7df6f1dac89f762142
francoragout <francoragout1@gmail.com>
2024-11-28 21:49:52 -0300

    microservices architecture

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { columns } from "@/components/columns";
 import { DataTable } from "@/components/data-table";
-import { taskSchema } from "@/db/schema";
+import { taskSchema, userSchema } from "@/db/schema";
 import { z } from "zod";
 
 type Task = z.infer<typeof taskSchema>;
+type User = z.infer<typeof userSchema>;
 
 async function GetTasks() {
   const response = await fetch("http://localhost:8000/api/tasks", {
@@ -12,11 +13,22 @@ async function GetTasks() {
   return await response.json();
 }
 
+async function GetUsers() {
+  const response = await fetch("http://localhost:5000/api/users", {
+    cache: "no-cache",
+  });
+  return await response.json();
+}
+
 export default async function Home() {
   const tasks = await GetTasks();
   const data = tasks.map((task: Task) => taskSchema.parse(task));
+  const users = await GetUsers();
+  const usersData = users.map((user: User) => userSchema.parse(user));
+
   return (
     <div className="container mx-auto mt-4">
+      <h1 className="text-2xl font-bold">This is a task table for {usersData}</h1>
       <DataTable columns={columns} data={data} />
     </div>
   );
--- db/schema.ts
+++ db/schema.ts
@@ -10,4 +10,12 @@ export const taskSchema = z.object({
   priority: z.string(),
 })
 
+export const userSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string(),
+})
+
+export type User = z.infer<typeof userSchema>
+
 export type Task = z.infer<typeof taskSchema>
